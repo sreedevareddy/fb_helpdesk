a4eb09eb1ec6b9262656ed95be8f777d2de59d42
Sreedeva <79521521+sreedevareddy@users.noreply.github.com>
2024-02-22 14:09:32 +0530

    cors

--- routes/Auth.js
+++ routes/Auth.js
@@ -5,6 +5,10 @@ const bcrypt = require('bcryptjs');
 // const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
 // Route for user registration
 router.post('/register', async (req, res) => {
   const { userName, email, password } = req.body;
