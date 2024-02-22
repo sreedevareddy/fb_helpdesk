a0a7808d6545ed3f412fc104f5ad9df994ed8b8c
Sreedeva <79521521+sreedevareddy@users.noreply.github.com>
2024-02-22 13:47:12 +0530

    cors

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
-const facebookRoutes = require('./routes/Facebook');
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const facebookRoutes = require("./routes/Facebook");
 
 const app = express();
 app.use(cors());
+app.use(
+  cors({
+    origin: "https://fb-helpdesk-server.vercel.app/",
+  })
+  
+);
 
 // Connect Database
 connectDB();
@@ -13,8 +19,8 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // Define Routes
-app.use('/api/auth', require('./routes/Auth'));
-app.use('/api/facebook', facebookRoutes);
+app.use("/api/auth", require("./routes/Auth"));
+app.use("/api/facebook", facebookRoutes);
 
 const PORT = process.env.PORT || 5000;
 
